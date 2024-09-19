e68b85b4863282151cabf8bca06c39ed81c4f015
NoelLaura <lauranoel2002@gmail.com>
2024-09-19 18:14:14 +0200

    dec2bin.py ya no funciona

--- dec2bin.py
+++ dec2bin.py
@@ -7,7 +7,7 @@
 # ------------------------------------------------------
 def dec2bin(numero_decimal, numero_bits):
     numero_binario = bin(numero_decimal)
-   if numero_decimal >= 0:
+   if numero_decimal >= 0
     numero_binario = numero_binario[2:len(numero_binario)]  # quita el "0b" del principio
     
     while len(numero_binario) < numero_bits:      # añade 0's a la izquierda si hace falta
@@ -17,7 +17,6 @@ def dec2bin(numero_decimal, numero_bits):
         while len(numero_binario) < numero_bits:    # añade 1's a la izquierda si hace falta
             numero_binario = "1" + numero_binario
     return numero_binario
-    
 
 # ----------------------------------------
 # MAIN
